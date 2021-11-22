27d99d626ff512619c2c7400a5978c17354b5fe8
k8c <shinisgood@yerzui-MacBookAir.local>
2021-11-22 18:51:20 +0900

    모델링 진행중

--- matches/models.py
+++ matches/models.py
@@ -1,28 +1,32 @@
 from django.db import models
 
 # Create your models here.
-class City(models.Model):
-    idx = models.AutoField(primary_key=True)
+class Region(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, blank=True, null=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = "cities"
+        db_table = "regions"
         app_label = "matches"
 
 
-class Question(models.Model):
+class District(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey("user.User", models.DO_NOTHING, null=False)
-    product_category = models.ForeignKey(
-        "articles.ArticleProductCategory", models.DO_NOTHING, null=True, blank=True
-    )
-    value_category = models.ForeignKey(
-        "articles.ArticleValueCategory", models.DO_NOTHING, null=True, blank=True
-    )
-    description = models.CharField(max_length=1500, blank=True, null=True)
+    name = models.CharField(max_length=30, blank=True, null=True)
 
     class Meta:
-        db_table = "questions"
-        app_label = "questions"
+        db_table = "districts"
+        app_label = "matches"
+
+
+class Match(models.Model):
+    id = models.AutoField(primary_key=True)
+    city = models.ForeignKey("City", models.DO_NOTHING, null=True, blank=True)
+    date = models.DateTimeField()
+    schedule = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "matches"
+        app_label = "matches"
--- users/models.py
+++ users/models.py
@@ -27,11 +27,44 @@ class User(AbstractUser):
     bank_name = models.CharField(max_length=50, null=True, blank=True)
     bank_account = models.CharField(max_length=50, null=True, blank=True)
     bank_account_holder = models.CharField(max_length=50, null=True, blank=True)
-    manner = models.IntegerField(null=True, blank=True)
+    fairplay_point = models.IntegerField(null=True, blank=True)
+    style = models.CharField(max_length=20, choices=PLAY_TPYE, null=True, blank=True)
+    strength = models.CharField(max_length=50, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_newbie = models.BooleanField(default=True)
+    is_vaccinated = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = "users"
+        app_label = "users"
+
+
+class Applier(AbstractUser):
+    username = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_newbie = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = "users"
+        app_label = "users"
+
+
+class Level(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey("User", models.DO_NOTHING, null=True, blank=True)
+    match = models.ForeignKey("matches.Match", models.DO_NOTHING, null=True, blank=True)
+    manager = models.ForeignKey("Manager", models.DO_NOTHING, null=True, blank=True)
     level = models.DecimalField(max_digits=8, decimal_places=1, null=True, blank=True)
-    play_type = models.CharField(
-        max_length=20, choices=PLAY_TPYE, null=True, blank=True
-    )
+    style = models.CharField(max_length=20, choices=PLAY_TPYE, null=True, blank=True)
+    strength = models.CharField(max_length=50, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
